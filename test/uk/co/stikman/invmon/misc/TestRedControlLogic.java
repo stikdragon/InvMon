527b1423b8b9a2ec9b86be928c522df64e8d1941
Stik <stikdragon@gmail.com>
2023-09-24 23:16:23 +0100

    tests

--- test/uk/co/stikman/invmon/misc/TestRedControlLogic.java
+++ test/uk/co/stikman/invmon/misc/TestRedControlLogic.java
@@ -1,20 +1,46 @@
 package uk.co.stikman.invmon.misc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import uk.co.stikman.invmon.InvMonException;
 import uk.co.stikman.invmon.controllers.RedControllerLogic;
+import uk.co.stikman.invmon.inverter.util.InvUtil;
+import uk.co.stikman.invmon.util.TestUtils;
 
 public class TestRedControlLogic {
-	@Test
-	public void testStuff() {
-		RedControllerLogic x = new RedControllerLogic(null);
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-		
-		x.run(LocalDateTime.parse("2023-01-04 15:00"));
-        
+	private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
+	@Test
+	public void testStuff() throws InvMonException, IOException {
+		Element el = loadTestConfigSection("Test1");
+		RedControllerLogic x = new RedControllerLogic(null);
+		File f = TestUtils.resourceToTempFile(this.getClass(), "red_csv_sample1.csv");
+		el.setAttribute("csv", f.getAbsolutePath());
+		x.config(el);
+
+		x.run(LocalDateTime.parse("2023-01-04 15:00", dtf));
+
+	}
+
+	private Element loadTestConfigSection(String id) throws IOException {
+		try (InputStream is = TestRedControlLogic.class.getResourceAsStream("controllertests1.xml")) {
+			Document root = InvUtil.loadXML(is);
+			for (Element el : InvUtil.getElements(root.getDocumentElement())) {
+				if (el.getTagName().equals(id))
+					return el;
+			}
+			throw new NoSuchElementException(id);
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
 	}
 }
--- test/uk/co/stikman/invmon/util/TestUtils.java
+++ test/uk/co/stikman/invmon/util/TestUtils.java
@@ -1,9 +1,16 @@
 package uk.co.stikman.invmon.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.teavm.apachecommons.io.FileUtils;
 
 import uk.co.stikman.invmon.inverter.util.InvUtil;
+import uk.co.stikman.utils.StreamUtil;
 
 public class TestUtils {
 	@Test
@@ -12,5 +19,13 @@ public class TestUtils {
 		Assert.assertEquals("1500 Kb", InvUtil.formatSize(1500 * 1024));
 		Assert.assertEquals("4096 Kb", InvUtil.formatSize(4 * 1024 * 1024));
 		Assert.assertEquals("9 Mb", InvUtil.formatSize(9 * 1024 * 1024));
+	}
+
+	public static File resourceToTempFile(Class<?> cls, String name) throws IOException {
+		try (InputStream is = cls.getResourceAsStream(name)) {
+			File f = File.createTempFile("invmontests", null);
+			FileUtils.copyInputStreamToFile(is, f);
+			return f;
+		}
 	}
 }
